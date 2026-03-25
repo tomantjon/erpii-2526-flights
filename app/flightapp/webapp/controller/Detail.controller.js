1c66af35061b8b718fdce04a4eb5db935f78a828
Tom Antjon <tom.antjon@hogent.be>
2026-03-25 16:58:04 +0100

    [App] Implement flight detail display fragment and initialize form in detail view

--- app/flightapp/webapp/controller/Detail.controller.js
+++ app/flightapp/webapp/controller/Detail.controller.js
@@ -5,6 +5,10 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
     onInit() {
       this.oRouter = this.getOwnerComponent().getRouter();
       this.oRouter.getRoute("DetailRoute").attachPatternMatched(this._onRouteMatched, this);
+
+      this.byId("edit").setEnabled(true);
+      // Set the initial form to be the display one
+      this._showFormFragment("FlightDetailDisplay");
     },
     _onRouteMatched: function (oEvent) {
       console.log("Route matched");
@@ -14,6 +18,19 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
 
       //bind the context of the base model to a specific flight, and not to the full entity /Flight
       this.getView().bindElement(flightpath);
+    },
+
+    _showFormFragment: function (sFragmentName) {
+      var oPage = this.byId("pgDetail");
+      oPage.removeAllContent();
+
+      this.pFormFragment = this.loadFragment({
+        name: "sap.capire.app.flightapp.fragment." + sFragmentName,
+      });
+
+      this.pFormFragment.then(function (oVBox) {
+        oPage.insertContent(oVBox);
+      });
     },
   });
 });
